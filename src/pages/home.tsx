c6c498e09023d92b36f1b203bf226a4909556157
1446450047 <1446450047@qq.com>
2022-05-20 17:10:49 +0800

    add header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './home.scss';
-import header from "../images/header.png"
+import headerBg from "../images/header.png"
 
 export const Home = () => {
   return (
     <div>
-        <div className="x"></div>
+        <header style={{backgroundImage: `url(${headerBg})`}}/>
     </div>
   );
 };
