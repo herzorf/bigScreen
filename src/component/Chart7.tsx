6428f3dab699e29cf16586e714bd2650fbd575cc
1446450047 <1446450047@qq.com>
2022-05-22 19:04:14 +0800

    完成 Chart7.tsx

--- src/component/Chart7.tsx
+++ src/component/Chart7.tsx
@@ -1,12 +1,62 @@
 import React, {useEffect, useRef} from "react";
 import * as echarts from "echarts";
+import {px} from "../shared/help";
 
 export const Chart7 = ()=>{
     const divRef = useRef(null);
     useEffect(() => {
         const myChart = echarts.init(divRef.current);
+        myChart.setOption({
+            color: '#F7A110',
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: [0, 18, 28, 38, 48, 58, 68, 78],
+                splitLine: {show: true, lineStyle: {color: '#073E78'}},
+                axisTick: {show: false},
+                axisLine: {show: false},
+            },
+            yAxis: {
+                type: 'value',
+                splitLine: {lineStyle: {color: '#073E78'}},
+                axisLabel: {
+                    formatter(val) {
+                        return val * 100 + '%';
+                    }
+                }
+            },
+            series: [{
+                type: 'line',
+                data: [
+                    0.19, 0.20, 0.26,
+                    0.35, 0.26, 0.20,
+                    0.08, 0.06
+                ],
+                symbol: 'circle',
+                symbolSize: px(12),
+                lineStyle: {width: px(2)},
+                areaStyle: {
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                        offset: 0,
+                        color: '#F7A110'
+                    }, {
+                        offset: 1,
+                        color: '#1B1D52'
+                    }]),
+                }
+            }],
+            grid:{
+                x:px(70),
+                y:px(30),
+                x2:px(20),
+                y2:px(40)
+            }
+        })
     }, []);
     return (
-        <div ref={divRef} className="chart">7</div>
+        <div>
+            <h3>犯罪年龄趋势图</h3>
+            <div ref={divRef} className="chart"/>
+        </div>
     )
 }
