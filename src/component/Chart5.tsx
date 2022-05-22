cba1d03b6e1017955e0e8f436473169493e05ba9
1446450047 <1446450047@qq.com>
2022-05-22 18:39:08 +0800

    完成 Chart5.tsx

--- src/component/Chart5.tsx
+++ src/component/Chart5.tsx
@@ -1,12 +1,52 @@
 import React, {useEffect, useRef} from "react";
 import * as echarts from "echarts";
+import {px} from "../shared/help";
 
-export const Chart5 = ()=>{
+export const Chart5 = () => {
     const divRef = useRef(null);
     useEffect(() => {
         const myChart = echarts.init(divRef.current);
+        myChart.setOption(
+            {
+                color: ["#8D70F8", "#33A4FA"],
+                xAxis: {show: false},
+                yAxis: {show: false},
+                legend: {
+                    show: true,
+                    bottom: "center",
+                    textStyle: {
+                        color: "#fff"
+                    }
+                },
+                series: [
+                    {
+                        name: "访问来源",
+                        type: "pie",
+                        radius: ["75%", "90%"],
+                        label: {
+                            show: true, position: "inside", textStyle: {color: "white", fontSize: px(20)},
+                            formatter(options) {
+                                return options.value * 100 + "%";
+                            }
+                        },
+                        itemStyle: {
+                            borderColor: "#0F113A",
+                            borderWidth: px(4)
+                        },
+                        data: [
+                            {value: 0.2, name: "女"},
+                            {value: 0.8, name: "男"},
+                        ]
+                    }],
+                grid: {
+                    x: px(40),
+                    y: px(60),
+                    x2: px(20),
+                    y2: px(200)
+                },
+            });
     }, []);
     return (
-        <div  ref={divRef} className="chart">5</div>
-    )
-}
+        <div ref={divRef} className="chart">5</div>
+    );
+};
