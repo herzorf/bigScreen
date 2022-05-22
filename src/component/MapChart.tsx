1ef075a7b6be0927755d1e28c29e37cd67d15ecd
1446450047 <1446450047@qq.com>
2022-05-22 13:05:54 +0800

    finish chinaMap code

--- src/component/MapChart.tsx
+++ src/component/MapChart.tsx
@@ -13,49 +13,97 @@ export const MapChart = () => {
         myChart.setOption({
             textStyle: {
                 fontSize: px(18),
-                color: '#79839E'
+                color: "#79839E"
             },
             title: {show: false},
-            legend: {show: true},
+            legend: {},
             grid: {
                 x: px(20),
                 y: px(20),
                 x2: px(20),
                 y2: px(20),
-                containLabel: true
             },
             series: [
                 {
+                    name: "map1",
                     type: "map",
                     map: "CN", // 自定义扩展图表类型
                     data: [
                         {name: "甘肃省", value: 1},
                     ],
+                    label: {show: false, color: "white"},
                     itemStyle: {
                         areaColor: "#010D3D",
                         color: colors["甘肃省"],
                         borderColor: "#01A7F7",
+
+                    },
+                    emphasis: {
+                        label: {
+                            show: true,
+                            color: "white"
+                        },
+                        areaColor: "#5470C6",
                     },
                 },
                 {
+                    name: "map1",
                     type: "map",
                     map: "CN", // 自定义扩展图表类型
                     data: [
                         {name: "四川省", value: 100},
                     ],
+                    itemStyle: {
+                        areaColor: "#010D3D",
+                        color: colors["四川省"],
+                        borderColor: "yellow",
+
+                    },
+                    emphasis: {
+                        label: {
+                            show: true,
+                            color: "white"
+                        },
+                        areaColor: "#5470C6",
+                    },
                 },
                 {
+                    name: "map1",
                     type: "map",
                     map: "CN", // 自定义扩展图表类型
                     data: [
                         {name: "青海省", value: 100},
                     ],
+                    itemStyle: {
+                        areaColor: "#010D3D",
+                        color: colors["青海省"],
+                        borderColor: "#01A7F7",
+                    },
+                    emphasis: {
+                        label: {
+                            show: true,
+                            color: "white"
+                        },
+                        areaColor: "#5470C6",
+                    },
                 },
 
             ]
         });
     }, []);
     return (
-        <div ref={divRef} className="chart"/>
+
+        <>
+            <div ref={divRef} className="chart"/>
+
+            <div className="wrapper">
+                <div className="legend bordered">
+                    <span className="icon" style={{background: colors["甘肃省"]}}/>甘肃籍
+                    <span className="icon" style={{background: colors["四川省"]}}/>四川籍
+                    <span className="icon" style={{background: colors["青海省"]}}/>青海籍
+                </div>
+                <div className="notes">此地图仅显示了中国的部分区域</div>
+            </div>
+        </>
     );
 };
