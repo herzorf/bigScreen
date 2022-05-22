cefe8ae9efe0df90fed8fe383408a7e63bf66724
1446450047 <1446450047@qq.com>
2022-05-22 21:30:10 +0800

    完成chart9 rightChart

--- src/component/Chart9.tsx
+++ src/component/Chart9.tsx
@@ -5,7 +5,6 @@ import {px} from "../shared/help";
 export const Chart9 = () => {
     const divRefLeft = useRef(null);
     const divRefRight = useRef(null);
-    const colors = ['#F46064', '#F38E1C', '#1CDB7C', '#8D70F8', '#33A4FA'];
     const data : Array<object> = [
         {value: 0.08, name: '东岗路'},
         {value: 0.06, name: '段家滩'},
@@ -56,7 +55,76 @@ export const Chart9 = () => {
         });
     }, []);
 
+    const data1 = [
+        {value: 0.08, name: '东岗路'},
+        {value: 0.06, name: '段家滩'},
+        {value: 0.11, name: '雁北'},
+        {value: 0.09, name: '五泉山'},
+        {value: 0.12, name: '中山路'},
+        {value: 0.06, name: '庆阳路'},
+        {value: 0.08, name: '武都路'},
+        {value: 0.08, name: '酒泉路'},
+        {value: 0.08, name: '天水路'},
+    ];
+    useEffect(() => {
+        const myChart = echarts.init(divRefRight.current);
+        myChart.setOption({
+            textStyle: {
+                color: "#fff"
+            },
+            xAxis: {
+                // @ts-ignore
+                data: data.map(i => i.name),
+                axisTick: {show: false},
+                axisLine: {
+                    lineStyle: {color: '#083B70'}
+                },
+                axisLabel: {
+                    formatter(val) {
+                        if (val.length > 2) {
+                            const array = val.split('');
+                            array.splice(2, 0, '\n');
+                            return array.join('');
+                        } else {
+                            return val;
+                        }
+                    }
+                },
+            },
 
+            yAxis: {
+                splitLine: {show: false},
+                axisLine: {
+                    show: true,
+                    lineStyle: {color: '#083B70'}
+                },
+                axisLabel: {
+                    formatter(value) {
+                        return (value * 100).toFixed(0) + '%';
+                    }
+                }
+            },
+            series: [{
+                type: 'bar',
+                // @ts-ignore
+                data: data1.map(i => i.value),
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    offset: 0,
+                    color: '#0A97FB'
+                }, {
+                    offset: 1,
+                    color: '#1E34FA'
+                }]),
+            }],
+            grid:{
+                x:px(60),
+                y:px(25),
+                x2:px(20),
+                y2:px(50),
+
+            }
+        });
+    }, []);
     return (
         <div>
             <div className="chart" ref={divRefLeft}/>
